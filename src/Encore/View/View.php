39532cc57096beceb799e24f9ac1e37337fa06fd
Chris Harvey <chris@chrisnharvey.com>
2013-11-30 14:31:42 +0000

    Updates to the view class

--- src/Encore/View/View.php
+++ src/Encore/View/View.php
@@ -2,15 +2,23 @@
 
 namespace Encore\View;
 
+use Encore\View\Object\Frame;
+
 class View
 {
-    public function listen($event, $callback)
+    public function __construct($path)
     {
-
+        // Initialize wxXmlResource and load the view
+        $xrc = new wxXmlResource();
+        wxXmlResource::Get()->InitAllHandlers();
+        wxXmlResource::Get()->Load($path);
     }
 
     public function getFrame($frame)
     {
-        
+        $frame = new wxFrame;
+        wxXmlResource::Get()->LoadFrame($frame, null, $frame);
+
+        return new Frame($frame);
     }
 }
