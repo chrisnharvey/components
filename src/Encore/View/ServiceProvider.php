f8cd64257014f9f1c79de1f2d3541bc0d1a0d540
Chris Harvey <chris@chrisnharvey.com>
2013-12-09 12:40:34 +0000

    Correctly binding the view into the IoC container

--- src/Encore/View/ServiceProvider.php
+++ src/Encore/View/ServiceProvider.php
@@ -2,10 +2,18 @@
 
 namespace Encore\View;
 
+use Illuminate\Filesystem\Filesystem;
+
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     public function register()
     {
-        $this->app->instance('view', new Manager);
+        $app = $this->app;
+
+        $this->app->instance('view', function() use ($app) {
+            $finder = new FileFinder(new Filesystem, $app['config']['view.paths']);
+
+            return new Manager($finder);
+        });
     }
 }
