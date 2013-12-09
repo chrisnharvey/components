839c141d749736dcbf53e132aa7242c0505e6d8f
Chris Harvey <chris@chrisnharvey.com>
2013-12-09 12:40:50 +0000

    Updating view manager to use the view finder

--- src/Encore/View/Manager.php
+++ src/Encore/View/Manager.php
@@ -4,8 +4,15 @@ namespace Encore\View;
 
 class Manager
 {
+    public function __construct(FinderInterface $finder)
+    {
+        $this->finder = $finder;
+    }
+
     public function make($view)
     {
-        return new View($view);
+        $path = $this->finder->find($view);
+
+        return new View($path);
     }
 }
