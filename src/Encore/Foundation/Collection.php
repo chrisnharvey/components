c43cf15cd3f2b27028a3ce2f575f598000a99354
Chris Harvey <chris@chrisnharvey.com>
2013-12-18 11:05:05 +0000

    Adding collection class

---
+++ src/Encore/Foundation/Collection.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Encore\Foundation;
+
+class Collection implements \ArrayAccess
+{
+    protected $collection = array();
+
+    public function offsetSet($offset, $value)
+    {
+        if (is_null($offset)) {
+            $this->collection[] = $value;
+        } else {
+            $this->collection[$offset] = $value;
+        }
+    }
+
+    public function offsetExists($offset)
+    {
+        return isset($this->collection[$offset]);
+    }
+
+    public function offsetUnset($offset)
+    {
+        unset($this->collection[$offset]);
+    }
+
+    public function offsetGet($offset)
+    {
+        return isset($this->collection[$offset]) ? $this->collection[$offset] : null;
+    }
+}
