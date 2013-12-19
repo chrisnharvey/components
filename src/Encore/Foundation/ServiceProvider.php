2ebca2a8afb483505d9cce450cda8bac9265b7ca
Chris Harvey <chris@chrisnharvey.com>
2013-12-19 14:52:23 +0000

    Adding service provider abstract class

---
+++ src/Encore/Foundation/ServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Encore\Foundation;
+
+abstract class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register() {}
+}
