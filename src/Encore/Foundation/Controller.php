c18feedb9e2a224017af6cc5c1314a34da8939f0
Chris Harvey <chris@chrisnharvey.com>
2013-12-22 18:34:46 +0000

    Refactoring controller destructor

--- src/Encore/Foundation/Controller.php
+++ src/Encore/Foundation/Controller.php
@@ -4,21 +4,9 @@ namespace Encore\Foundation;
 
 abstract class Controller
 {
-    public $data = array();
-
-    public function __set($property, $value)
-    {
-        $this->data[$property] = $value;
-    }
-
-    public function __get($property)
-    {
-        return $this->data[$property];
-    }
-
     public function __destruct()
     {
         $collection = \App::make('collection');
-        $collection[] = $this->data;
+        $collection[] = get_object_vars($this);
     }
 }
