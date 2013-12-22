907d1685c4fab66d6b2b854e0d04aafe672b343c
Chris Harvey <chris@chrisnharvey.com>
2013-12-22 18:39:22 +0000

    Refactoring to make only public properties static

--- src/Encore/Foundation/Controller.php
+++ src/Encore/Foundation/Controller.php
@@ -7,6 +7,16 @@ abstract class Controller
     public function __destruct()
     {
         $collection = \App::make('collection');
-        $collection[] = get_object_vars($this);
+
+        // Assign the object to another var
+        $that = $this;
+
+        // Change the scope (kinda).
+        $savable = function() use ($that) {
+            return get_object_vars($that);
+        };
+
+        // Save public properties in the collection
+        $collection[] = $savable(); 
     }
 }
