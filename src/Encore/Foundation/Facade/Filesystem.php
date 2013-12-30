b4b4366143925cbdaf91d9e5d30a6b40d35c7fc7
Chris Harvey <chris@chrisnharvey.com>
2013-12-30 13:58:26 +0000

    Adding filesystem facade

---
+++ src/Encore/Foundation/Facade/Filesystem.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Encore\Foundation\Facade;
+
+class Filesystem extends \Encore\Foundation\Facade
+{
+    public static function getFacade()
+    {
+        return 'filesystem';
+    }
+}
