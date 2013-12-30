bb16a54068445362c6f09e59139b15fea5603ff0
Chris Harvey <chris@chrisnharvey.com>
2013-12-30 13:57:50 +0000

    Renaming the IoC instance of filesystem

--- src/Encore/Foundation/Provider/Filesystem.php
+++ src/Encore/Foundation/Provider/Filesystem.php
@@ -6,7 +6,7 @@ class Filesystem extends \Encore\Foundation\ServiceProvider
 {
     public function register()
     {
-        $this->app['files'] = $this->app->share(function() {
+        $this->app['filesystem'] = $this->app->share(function() {
             return new Filesystem;
         });
     }
