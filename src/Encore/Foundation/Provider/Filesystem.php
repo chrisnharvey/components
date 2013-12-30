fdc7cd1f3d265ee911eb3738c52fcde2156806d2
Chris Harvey <chris@chrisnharvey.com>
2013-12-30 21:18:25 +0000

    Fixing filesystem service provider

--- src/Encore/Foundation/Provider/Filesystem.php
+++ src/Encore/Foundation/Provider/Filesystem.php
@@ -6,8 +6,8 @@ class Filesystem extends \Encore\Foundation\ServiceProvider
 {
     public function register()
     {
-        $this->app['filesystem'] = $this->app->share(function() {
-            return new Filesystem;
+        $this->app->bind('filesystem', function() {
+            return new \Illuminate\Filesystem\Filesystem;
         });
     }
 }
