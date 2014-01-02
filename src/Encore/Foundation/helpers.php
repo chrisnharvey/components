56da15478a0392fa7b3e45daeff779cef9c9dfa9
Chris Harvey <chris@chrisnharvey.com>
2014-01-02 15:39:31 +0000

    Several fixes for curdir() function

--- src/Encore/Foundation/helpers.php
+++ src/Encore/Foundation/helpers.php
@@ -10,5 +10,10 @@ function curdir()
 
     if (empty($phar)) return $dir;
 
-    return \Phar::running().str_replace(dirname($phar), '', $dir);
+    $dir = str_replace('phar://', '', $dir);
+
+    $dir = str_replace($phar, '', $dir);
+    $dir = str_replace(dirname($phar), '', $dir);
+
+    return \Phar::running().$dir;
 }
