42ded3bd6a02b4869dd121ca494bfa5a695d46f3
Chris Harvey <chris@chrisnharvey.com>
2014-01-02 10:12:03 +0000

    Adding logic for getting the current directory from within a phar

--- src/Encore/Foundation/helpers.php
+++ src/Encore/Foundation/helpers.php
@@ -4,5 +4,11 @@ function curdir()
 {
     $trace = debug_backtrace(0, 1);
 
-    return dirname($trace[0]['file']);
+    $dir = dirname($trace[0]['file']);
+
+    $phar = \Phar::running(false);
+
+    if (empty($phar)) return $dir;
+
+    return \Phar::running().str_replace(dirname($phar), '', $dir);
 }
