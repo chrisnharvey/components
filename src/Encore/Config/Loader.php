7c4a35c0be204edf328d1e8d3e890a01599cc6e3
Chris Harvey <chris@chrisnharvey.com>
2014-01-14 17:27:46 +0000

    Mode, not environment

--- src/Encore/Config/Loader.php
+++ src/Encore/Config/Loader.php
@@ -15,13 +15,13 @@ class Loader extends \Illuminate\Config\FileLoader
         parent::__construct($files, $defaultPath);
     }
 
-    public function load($environment, $group, $namespace = null)
+    public function load($mode, $group, $namespace = null)
     {
         $items = array();
 
-        // First we'll get the root configuration path for the environment which is
+        // First we'll get the root configuration path for the mode which is
         // where all of the configuration files live for that namespace, as well
-        // as any environment folders with their specific configuration items.
+        // as any mode folders with their specific configuration items.
         $path = $this->getPath($namespace);
 
         if (is_null($path)) {
@@ -29,7 +29,7 @@ class Loader extends \Illuminate\Config\FileLoader
         }
 
         // First we'll get the main configuration file for the groups. Once we have
-        // that we can check for any environment specific files, which will get
+        // that we can check for any mode specific files, which will get
         // merged on top of the main arrays to make the environments cascade.
         $file = "{$path}/{$group}.php";
 
@@ -44,25 +44,25 @@ class Loader extends \Illuminate\Config\FileLoader
             $items = $this->mergeEnvironment($items, $file);
         }
 
-        // Check for a global environment config file (not OS specific)
-        $file = "{$path}/{$environment}/{$group}.php";
+        // Check for a global mode config file (not OS specific)
+        $file = "{$path}/{$mode}/{$group}.php";
 
         if ($this->files->exists($file)) {
             $items = $this->mergeEnvironment($items, $file);
         }
 
-        // And lastly we'll check if we have an environment and OS specific
+        // And lastly we'll check if we have an mode and OS specific
         // config file and merge.
-        $file = "{$path}/{$this->os}/{$environment}/{$group}.php";
+        $file = "{$path}/{$this->os}/{$mode}/{$group}.php";
 
         if ($this->files->exists($file)) {
-            $items = $this->mergeEnvironment($items, $file);
+            $items = $this->mergeMode($items, $file);
         }
 
         return $items;
     }
 
-    protected function mergeEnvironment(array $items, $file)
+    protected function mergeMode(array $items, $file)
     {
         return array_merge_recursive($items, $this->files->getRequire($file));
     }
