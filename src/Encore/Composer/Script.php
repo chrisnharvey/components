d1d288d242611a6474de541d7000de60e90083bf
Chris Harvey <chris@chrisnharvey.com>
2014-01-19 11:29:50 +0000

    Removing deadweight

--- src/Encore/Composer/Script.php
+++ src/Encore/Composer/Script.php
@@ -18,55 +18,11 @@ class Script
         static::$vendorPath = \App::vendorPath();
         static::$resourcesPath = \App::resourcesPath();
 
-        if (defined('COMPILING') and COMPILING) {
+        if (defined('COMPILING')) {
             static::$appPath = BUILD_PATH.'/app';
             static::$vendorPath = BUILD_PATH.'/vendor';
             static::$resourcesPath = BUILD_PATH.'/resources';
         }
-    }
-
-    public static function postPackageInstall(Event $event)
-    {
-        // static::init();
-
-        // $package = $event->getOperation()->getPackage();
-
-        // $data = $package->getExtra();
-
-        // if ( ! array_key_exists('resources', $data)) return;
-
-        // $name = $package->getName();
-
-        // foreach ($data['resources'] as $resource => $)
-        // $path = static::$vendorPath."/{$name}/";
-
-        // static::$resources[$name] = $path;
-
-       // var_dump($path); exit;
-
-        // if ( ! array_key_exists('paths', $data)) return;
-
-        // if (array_key_exists('view', $data['paths'])) {
-        //     foreach ($data['paths']['view'] as $namespace => $path) {
-                
-        //     }
-        // }
-
-        // if (array_key_exists('asset', $data['paths'])) {
-        //     foreach ($data['paths']['asset'] as $namespace => $path) {
-
-        //     }
-        // }
-
-        // Get the namespace from extra
-        // Copy over the "view-path" and "asset-path" (if they are not set then use default)
-        // Copy them into an assets folder (/assets/namespaces/namespace)
-        // Standard assets will go into /assets/app/dir-name-sha1
-    }
-
-    public static function postPackageUpdate(Event $event)
-    {
-        return static::postPackageInstall($event);
     }
 
     public static function postInstall(Event $event)
