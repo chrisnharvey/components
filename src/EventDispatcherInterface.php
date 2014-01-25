4576afb4659d77daa02356bac2b77b2c42b47199
Chris Harvey <chris@chrisnharvey.com>
2014-01-25 11:12:48 +0000

    Adding doc blocks to EventDispatcherInterface

--- src/EventDispatcherInterface.php
+++ src/EventDispatcherInterface.php
@@ -4,5 +4,12 @@ namespace Encore\Container;
 
 interface EventDispatcherInterface
 {
+    /**
+     * Listen for a specific event to occur
+     *
+     * @param string $event
+     * @param Closure $trigger
+     * @return void
+     */
     public function listen($event, \Closure $trigger);
 }
