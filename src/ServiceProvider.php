75fa2e684d928d9a8e93781ecf7debcbc4c61f0a
Chris Harvey <chris@chrisnharvey.com>
2014-02-22 14:14:14 +0000

    Updates for new container

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,14 +4,10 @@ namespace Encore\Console;
 
 use Encore\Console\Application as Console;
 
-class ServiceProvider extends \Illuminate\Support\ServiceProvider
+class ServiceProvider extends \Encore\Container\ServiceProvider
 {
     public function register()
     {
-        $app = $this->app;
-
-        $this->app->singleton('console', function() use ($app) {
-            return new Console('EncorePHP', $app::VERSION);
-        });
+        $this->container->bind('console', new Console('EncorePHP', $this->container::VERSION));
     }
 }
