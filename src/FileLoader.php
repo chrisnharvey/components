c3179f8a1a4fd9b0c6d23f067135adb93dd014b7
Chris Harvey <chris@chrisnharvey.com>
2014-03-04 18:11:12 +0000

    Adding skeleton file loader implementation

---
+++ src/FileLoader.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Encore\Config;
+
+use Symfony\Component\Filesystem\Filesystem;
+
+class FileLoader
+{
+    public function __construct(Filesystem $fs)
+    {
+        $this->fs = $fs;
+    }
+
+    /**
+    * Load the given configuration group.
+    *
+    * @param string $environment
+    * @param string $group
+    * @param string $namespace
+    * @return array
+    */
+    public function load($environment, $group, $namespace = null)
+    {
+
+    }
+
+    /**
+    * Determine if the given configuration group exists.
+    *
+    * @param string $group
+    * @param string $namespace
+    * @return bool
+    */
+    public function exists($group, $namespace = null)
+    {
+
+    }
+
+    /**
+    * Add a new namespace to the loader.
+    *
+    * @param string $namespace
+    * @param string $hint
+    * @return void
+    */
+    public function addNamespace($namespace, $hint)
+    {
+
+    }
+
+    /**
+    * Returns all registered namespaces with the config
+    * loader.
+    *
+    * @return array
+    */
+    public function getNamespaces()
+    {
+
+    }
+}
