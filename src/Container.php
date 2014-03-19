470f09f95518fe9d7c0a8ce8bfb4c9b9d999b363
Chris Harvey <chris@chrisnharvey.com>
2014-03-19 18:39:18 +0000

    Merging original repo

--- src/Container.php
+++ src/Container.php
@@ -1,20 +1,31 @@
 <?php
+/**
+ * This file is part of the Encore\Container library.
+ * Originally part of the League\Di package.
+ *
+ * (c) Don Gilbert <don@dongilbert.net>
+ * (c) Chris Harvey <chris@chrisnharvey.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Encore\Container;
 
-use Closure;
-use ArrayAccess;
-use League\Di\Definition;
-use League\Di\Container as BaseContainer;
-
-class Container extends BaseContainer implements ArrayAccess
+/**
+ * Container class
+ *
+ * @author  Don Gilbert <don@dongilbert.net>
+ * @author  Chris Harvey <chris@chrisnharvey.com>
+ */
+class Container implements \ArrayAccess
 {
     /**
      * The event dispatcher implementation.
      *
-     * @var \Encore\Container\EventDispatcherInterface
+     * @var EventDispatcherInterface
      */
-    protected $event = null;
+    protected $event;
 
     /**
      * Array of service providers that resolve instances in the container
@@ -31,16 +42,48 @@ class Container extends BaseContainer implements ArrayAccess
     protected $registered = [];
 
     /**
+     * Array of container bindings.
+     *
+     * @var $array
+     */
+    protected $bindings = [];
+
+    /**
+     * The parent Container object.
+     *
+     * @var Container
+     */
+    protected $parent;
+
+    /**
      * Constructor
      *
-     * @param object $event \Encore\Container\EventDispatcherInterface
-     * @param object $parent \Encore\Container\Container
+     * @param object $parent Container
      */
-    public function __construct(EventDispatcherInterface $event = null, Container $parent = null)
+    public function __construct(Container $parent = null)
+    {
+        $this->parent = $parent;
+    }
+
+    /**
+     * Set the event dispatcher
+     *
+     * @param object $event EventDispatcherInterface
+     */
+    public function setEventDispatcher(EventDispatcherInterface $event)
     {
         $this->event = $event;
+    }
 
-        parent::__construct($parent);
+    /**
+     * Create a child Container with a new property scope that
+     * that has the ability to access the parent scope when resolving.
+     *
+     * @return Container
+     */
+    public function createChild()
+    {
+        return new static($this->event, $this);
     }
 
     /**
@@ -72,21 +115,46 @@ class Container extends BaseContainer implements ArrayAccess
     }
 
     /**
-     * Create a child Container with a new property scope that
-     * that has the ability to access the parent scope when resolving.
+     * Method to bind a concrete class to an abstract class or interface.
      *
-     * @return \Encore\Container\Container
+     * @param string          $abstract Class to bind.
+     * @param \Closure|string $concrete Concrete definition to bind to $abstract.
+     *
+     * @return Definition|\Closure The concrete class for adding method calls / constructor arguments if desired.
      */
-    public function createChild()
+    public function bind($abstract, $concrete = null)
     {
-        return new static($this->event, $this);
+        if (is_null($concrete)) {
+            $concrete = $abstract;
+        }
+
+        if ($this->shouldBeDefinitionObject($concrete)) {
+            $concrete = new Definition($this, $concrete);
+        }
+
+        return $this->bindings[$abstract] = $concrete;
+    }
+
+    /**
+     * Checks to see if a binding key has been bound in current Container.
+     * If not bound in the current Container, it will recursively search
+     * parent Container's until it finds the $binding.
+     *
+     * @param string $binding The binding to check.
+     * @return bool
+     */
+    public function bound($binding)
+    {
+        return isset($this->bindings[$binding]) || (isset($this->parent) && $this->parent->bound($binding));
     }
 
     /**
      * Build a concrete instance of a class.
      *
      * @param string $concrete The name of the class to buld.
+     *
      * @return mixed The instantiated class.
+     * @throws  \InvalidArgumentException
      */
     public function build($concrete)
     {
@@ -94,27 +162,21 @@ class Container extends BaseContainer implements ArrayAccess
            return $concrete; 
         }
 
-        return parent::build($concrete);
-    }
+        $reflection = new \ReflectionClass($concrete);
 
-    /**
-     * Bind a concrete class to an abstract class or interface.
-     *
-     * @param string $abstract Class to bind.
-     * @param mixed $concrete Concrete definition to bind to $abstract.
-     * Can be a \Closure, string or an instantiated object.
-     *
-     * @return mixed The concrete class for adding method calls / constructor arguments if desired.
-     */
-    public function bind($abstract, $concrete = null)
-    {
-        if ($this->objectNotClosure($concrete)) {
-            $concrete = new Definition($this, $concrete);
-
-            return $this->bindings[$abstract] = $concrete;
+        if (! $reflection->isInstantiable()) {
+            throw new \InvalidArgumentException(sprintf('Class %s is not instantiable.', $concrete));
         }
 
-        return parent::bind($abstract, $concrete);
+        $constructor = $reflection->getConstructor();
+
+        if (is_null($constructor)) {
+            return new $concrete;
+        }
+
+        $dependencies = $this->getDependencies($constructor);
+
+        return $reflection->newInstanceArgs($dependencies);
     }
 
     /**
@@ -144,89 +206,98 @@ class Container extends BaseContainer implements ArrayAccess
     }
 
     /**
+     * Extend an existing binding.
+     *
+     * @param   string   $binding  The name of the binding to extend.
+     * @param   \Closure $closure  The function to use to extend the existing binding.
+     *
+     * @return  void
+     * @throws  \InvalidArgumentException
+     */
+    public function extend($binding, \Closure $closure)
+    {
+        $rawObject = $this->getRaw($binding);
+
+        if (is_null($rawObject)) {
+            throw new \InvalidArgumentException(sprintf('Cannot extend %s because it has not yet been bound.', $binding));
+        }
+
+        $this->bind($binding, function ($container) use ($closure, $rawObject) {
+            return $closure($container, $rawObject($container));
+        });
+    }
+
+    /**
+     * Recursively build the dependency list for the provided method.
+     *
+     * @param \ReflectionMethod $method The method for which to obtain dependencies.
+     *
+     * @return array An array containing the method dependencies.
+     * @throws \InvalidArgumentException
+     */
+    protected function getDependencies(\ReflectionMethod $method)
+    {
+        $dependencies = [];
+
+        foreach ($method->getParameters() as $param) {
+            $dependency = $param->getClass();
+
+            if (is_null($dependency)) {
+                if ($param->isOptional()) {
+                    $dependencies[] = $param->getDefaultValue();
+                    continue;
+                }
+            } else {
+                $dependencies[] = $this->resolve($dependency->name);
+                continue;
+            }
+
+            throw new \InvalidArgumentException('Could not resolve ' . $param->getName());
+        }
+
+        return $dependencies;
+    }
+
+    /**
+     * Get the raw object prior to resolution.
+     *
+     * @param string $binding The $binding key to get the raw value from.
+     *
+     * @return Definition|\Closure Value of the $binding.
+     */
+    public function getRaw($binding)
+    {
+        if (isset($this->bindings[$binding])) {
+            return $this->bindings[$binding];
+        } elseif (isset($this->parent)) {
+            return $this->parent->getRaw($binding);
+        }
+
+        return null;
+    }
+
+    /**
      * Resolve the given binding.
      *
      * @param string $binding The binding to resolve.
+     *
      * @return mixed The results of invoking the binding callback.
      */
     public function resolve($binding)
     {
         $this->registerProvidersFor($binding);
 
-        return parent::resolve($binding);
-    }
+        $rawObject = $this->getRaw($binding);
 
-    /**
-     * Is a specified provider registered?
-     *
-     * @param ServiceProvider $provider The service provider object.
-     * @return bool
-     */
-    protected function providerIsRegistered(ServiceProvider $provider)
-    {
-        return in_array($provider, $this->registered);
-    }
+        // If the abstract is not registered, do it now for easy resolution.
+        if (is_null($rawObject)) {
+            // Pass $binding to both so it doesn't need to check if null again.
+            $this->bind($binding, $binding);
 
-    /**
-     * Register the events that register the specified provider
-     *
-     * @param ServiceProvider $provider The service provider object.
-     * @return void
-     */
-    protected function registerEvents(ServiceProvider $provider)
-    {
-        if (is_null($this->event)) return;
-
-        foreach ($provider->when() as $event) {
-            $this->event->listen($event, function() use ($provider) {
-                $this->registerProvider($provider, true);
-            });
+            $rawObject = $this->getRaw($binding);
         }
-    }
 
-    /**
-     * Register a service provider
-     *
-     * @param ServiceProvider $provider The service provider object.
-     * @param bool $force Force register (register whether needed or not)
-     * @return void
-     */
-    protected function registerProvider(ServiceProvider $provider, $force = false)
-    {
-        if ($this->providerIsRegistered($provider)) return;
-
-        $events = $provider->when();
-        $provides = $provider->provides();
-
-        if ($force or (empty($events) and empty($provides))) {
-            $provider->register();
-
-            $this->registered[] = $provider;
-        }
-    }
-
-    /**
-     * Remember the bindings that the specified service provider provides
-     *
-     * @param ServiceProvider $provider The service provider object.
-     * @return void
-     */
-    protected function registerProvides(ServiceProvider $provider)
-    {
-        foreach ($provider->provides() as $binding) {
-            $this->provides[$binding][] = $provider;
-        }
-    }
-
-    /**
-     * Check that the specified object is an object, but not a closure
-     *
-     * @param object $instance The object to check
-     * @return bool
-     */
-    protected function objectNotClosure($instance)
-    {
-        return is_object($instance) and ! $instance instanceof Closure;
+        return $rawObject($this);
     }
 
     /**
@@ -273,5 +344,82 @@ class Container extends BaseContainer implements ArrayAccess
     public function offsetUnset($offset)
     {
         unset($this->bindings[$offset]);
+    }
+
+    /**
+     * Is a specified provider registered?
+     *
+     * @param ServiceProvider $provider The service provider object.
+     * @return bool
+     */
+    protected function providerIsRegistered(ServiceProvider $provider)
+    {
+        return in_array($provider, $this->registered);
+    }
+
+    /**
+     * Register the events that register the specified provider
+     *
+     * @param ServiceProvider $provider The service provider object.
+     * @return void
+     */
+    protected function registerEvents(ServiceProvider $provider)
+    {
+        if ( ! isset($this->event)) return;
+
+        foreach ($provider->when() as $event) {
+            $this->event->listen($event, function() use ($provider) {
+                $this->registerProvider($provider, true);
+            });
+        }
+    }
+
+    /**
+     * Register a service provider
+     *
+     * @param ServiceProvider $provider The service provider object.
+     * @param bool $force Force register (register whether needed or not)
+     * @return void
+     */
+    protected function registerProvider(ServiceProvider $provider, $force = false)
+    {
+        if ($this->providerIsRegistered($provider)) return;
+
+        $events = $provider->when();
+        $provides = $provider->provides();
+
+        if ($force or (empty($events) and empty($provides))) {
+            $provider->register();
+
+            $this->registered[] = $provider;
+        }
+    }
+
+    /**
+     * Remember the bindings that the specified service provider provides
+     *
+     * @param ServiceProvider $provider The service provider object.
+     * @return void
+     */
+    protected function registerProvides(ServiceProvider $provider)
+    {
+        foreach ($provider->provides() as $binding) {
+            $this->provides[$binding][] = $provider;
+        }
+    }
+
+    /**
+     * Check if the specified concrete definiton should be a
+     * definition object.
+     *
+     * @param object $concrete The concrete definition
+     * @return bool
+     */
+    protected function shouldBeDefinitionObject($concrete)
+    {
+        return (
+            (is_object($concrete) and ! $concrete instanceof Closure)
+            or is_string($concrete)
+        );
     }
 }
