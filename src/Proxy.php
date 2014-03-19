d758dfdcf6d29c453eac57c87c4250c287fc878f
Chris Harvey <chris@chrisnharvey.com>
2014-03-19 18:50:30 +0000

    Adding copyright information to the top of additional files

--- src/Proxy.php
+++ src/Proxy.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * This file is part of the Encore\Container library.
+ * Inspired by Laravel Facades.
+ *
+ * (c) Chris Harvey <chris@chrisnharvey.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Encore\Container;
 
