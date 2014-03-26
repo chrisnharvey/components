24c9b1fd364aa8f35eac0b1921102909779f2918
Chris Harvey <chris@chrisnharvey.com>
2014-03-26 10:46:14 +0000

    Adding exceptions

---
+++ src/Exception/DuplicateIdException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Encore\GIML\Exception;
+
+class DuplicateIdException extends \Exception
+{
+    
+}
---
+++ src/Exception/InvalidElementException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Encore\GIML\Exception;
+
+class InvalidElementException extends \Exception
+{
+    
+}
