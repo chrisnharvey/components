c1371ec9467634c21ffdb756acdc575382f0daa1
Chris Harvey <chris@chrisnharvey.com>
2014-03-27 18:43:44 +0000

    Removing wxWidgets specific helpers

--- src/Kernel/helpers.php
+++ src/Kernel/helpers.php
@@ -17,20 +17,3 @@ function curdir()
 
     return \Phar::running().$dir;
 }
-
-function alert($message, $title = 'Alert')
-{
-    return \wxMessageBox($message, $title, wxOK|wxICON_INFORMATION);
-}
-
-function error($message, $title = 'Error')
-{
-    return \wxMessageBox($message, $title, wxOK|wxICON_ERROR);
-}
-
-function confirm($message, $title = 'Confirm')
-{
-    $answer = \wxMessageBox($message, $title, wxYES_NO);
-
-    return $answer === wxYES;
-}
