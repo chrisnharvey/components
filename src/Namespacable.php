eac783fbd744c443f134f8b14ff9410f76a7267a
Chris Harvey <chris@chrisnharvey.com>
2014-03-28 20:39:01 +0000

    Namespacable now uses Namespacable trait

--- src/Namespacable.php
+++ src/Namespacable.php
@@ -9,32 +9,5 @@ namespace Encore\Namespacer;
 
 abstract class Namespacable extends Resolver implements NamespacableInterface
 {
-    /**
-     * All of the namespace hints.
-     *
-     * @var array
-     */
-    protected $hints = [];
-
-    /**
-     * Add a namespace and provide a hint
-     *
-     * @param string $namespace
-     * @param string $hint
-     * @return void
-     */
-    public function addNamespace($namespace, $hint)
-    {
-        $this->hints[$namespace] = $hint;
-    }
-
-    /**
-     * Return an array of all the registered namespaces
-     *
-     * @return array
-     */
-    public function getNamespaces()
-    {
-        return $this->hints;
-    }
+    use NamespacableTrait;
 }
