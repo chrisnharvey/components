19c34aa10d90f60f35cb58aaff87e6b89af8f075
Chris Harvey <chris@chrisnharvey.com>
2014-03-29 14:59:08 +0000

    Adding command registration logic to console service provider

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Encore\Console;
 
 use Encore\Console\Application as Console;
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
 
 class ServiceProvider extends \Encore\Container\ServiceProvider
 {
@@ -10,5 +11,28 @@ class ServiceProvider extends \Encore\Container\ServiceProvider
     {
         $container = $this->container;
         $this->container->bind('console', new Console('EncorePHP', $container::VERSION));
+    }
+
+    public function boot()
+    {
+        // Loop the providers and register commands
+        $providers = $this->container->providers(false);
+
+        foreach ($providers as $provider) {
+            if (method_exists($provider, 'commands')) {
+                $this->registerCommands($provider->commands());
+            }
+        }
+    }
+
+    protected function registerCommands(array $commands)
+    {
+        foreach ($commands as $command) {
+            if ( ! $command instanceof SymfonyCommand) {
+                $command = $this->container[$command];
+            }
+
+            $this->container['console']->add($command);
+        }
     }
 }
