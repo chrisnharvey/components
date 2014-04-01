03caf17af00ee6815cd39313db615cdf3fc62690
Chris Harvey <chris@chrisnharvey.com>
2014-04-01 23:40:35 +0100

    Adding service provider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Encore\Development;
+
+class ServiceProvider extends \Encore\Container\ServiceProvider
+{
+    public function commands()
+    {
+        return ['Encore\Development\Command\Debug', 'Encore\Development\Command\Tinker'];
+    }
+}
