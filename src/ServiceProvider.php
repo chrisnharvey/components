727459bf4395003a7e74d3a649b447b3cbe6d000
Chris Harvey <chris@chrisnharvey.com>
2014-04-03 22:48:47 +0100

    No longer registering anything in service provider, just commands

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,13 +4,6 @@ namespace Encore\Resource;
 
 class ServiceProvider extends \Encore\Container\ServiceProvider
 {
-    public function register()
-    {
-        $this->container->bind('resource', function() {
-            return new Manager($this->container->resourcePath());
-        });
-    }
-
     public function commands()
     {
         return ['Encore\Resource\Command\Publish'];
