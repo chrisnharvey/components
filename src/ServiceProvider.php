313ae3e73b4a3ef491b55896e6986d4d49c05d9e
Chris Harvey <chris@chrisnharvey.com>
2014-08-10 12:17:52 +0100

    Adding service provider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Encore\Error;
+
+class ServiceProvider extends \Encore\Container\ServiceProvider
+{
+	public function register()
+	{
+		$this->container->bind('Encore\Error\ExceptionDisplayerInterface', 'Encore\Error\ConsoleDisplayer');
+		$this->container->bind('error', 'Encore\Error\Handler')
+			->withMethod('setContainer', [$this->container]);
+	}
+}
