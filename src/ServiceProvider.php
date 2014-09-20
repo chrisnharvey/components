e0a907e5d42456be6479aff2ea13ef6eca278e6d
Chris Harvey <chris@chrisnharvey.com>
2014-09-20 19:32:49 +0100

    Container aware classes inject the container automatically

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,7 +7,6 @@ class ServiceProvider extends \Encore\Container\ServiceProvider
 	public function register()
 	{
 		$this->container->bind('Encore\Error\ExceptionDisplayerInterface', 'Encore\Error\ConsoleDisplayer');
-		$this->container->bind('error', 'Encore\Error\Handler')
-			->withMethod('setContainer', [$this->container]);
+		$this->container->bind('error', 'Encore\Error\Handler');
 	}
 }
