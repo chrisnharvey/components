479565489a9d5a8c2373363beca3d2883bb4a0de
Chris Harvey <chris@chrisnharvey.com>
2014-09-24 11:42:43 +0100

    Adding register method that will register the dev config path

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,6 +4,11 @@ namespace Encore\Development;
 
 class ServiceProvider extends \Encore\Container\ServiceProvider
 {
+    public function register()
+    {
+        //$this->container['config']->addPath($this->container->basePath().'/dev');
+    }
+
     public function commands()
     {
         return ['Encore\Development\Command\Debug'];
