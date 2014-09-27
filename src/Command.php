4c3ec11218d5d81df2fc001cb2e67cdc12050864
Chris Harvey <chris@chrisnharvey.com>
2014-09-27 12:54:22 +0100

    Adding doc blocks

--- src/Command.php
+++ src/Command.php
@@ -7,6 +7,11 @@ class Command extends \Encore\Console\Command
     protected $name = 'test';
     protected $description = 'Run application unit tests';
 
+    /**
+     * Execute the command
+     * 
+     * @return void
+     */
     public function fire()
     {
         $_SERVER['argv'] = array_slice($_SERVER['argv'], 2);
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,6 +4,11 @@ namespace Encore\Testing;
 
 class ServiceProvider extends \Encore\Container\ServiceProvider
 {
+    /**
+     * Register the test command.
+     *
+     * @return void
+     */
     public function commands()
     {
         return ['Encore\Testing\Command'];
