b481a2bac59b3c3024e72e13b382b84669fd9191
Chris Harvey <chris@chrisnharvey.com>
2014-09-29 20:22:55 +0100

    Importing base service provider

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace Encore\Resource;
 
-class ServiceProvider extends \Encore\Container\ServiceProvider
+use Encore\Container\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
 {
     /**
      * Register the command for publishing resources.
