d463e222949cf0e0b1cac2caa8740fb6fbfc6a41
Chris Harvey <chris@chrisnharvey.com>
2015-02-27 16:55:16 +0000

    Adding empty database commands

---
+++ src/database/src/Command/GenerateModel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Encore\Database\Command;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+
+class GenerateModel extends \Encore\Console\Command
+{
+    protected $name = 'model:generate';
+    protected $description = 'Generate models based on schema';
+
+    /**
+     * Fire the install command
+     * 
+     * @return void
+     */
+    public function fire()
+    {
+        
+    }
+}
