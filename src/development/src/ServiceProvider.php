569fa22c66a4e3311d7cf53cce3351c14fe2a4ad
Chris Harvey <chris@chrisnharvey.com>
2015-03-07 14:08:11 +0000

    Register robo tasks into encore CLI

--- src/development/src/ServiceProvider.php
+++ src/development/src/ServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace Encore\Development;
 
+use TaskFile;
+use Robo\Runner;
+use Robo\Result;
+use Symfony\Component\Console\Input\InputInterface;
 use Encore\Container\ServiceProvider as BaseServiceProvider;
 
 class ServiceProvider extends BaseServiceProvider
@@ -27,6 +31,40 @@ class ServiceProvider extends BaseServiceProvider
      */
     public function commands()
     {
-        return ['Encore\Development\Command\Debug'];
+        return array_merge($this->getDevelopmentCommands(), [
+            'Encore\Development\Command\Debug'
+        ]);
+    }
+
+    protected function getDevelopmentCommands()
+    {
+        if ( ! class_exists('TaskFile')) return [];
+
+        $commands = [];
+
+        $tasks = new TaskFile;
+        $runner = new Runner;
+
+        $commandNames = array_filter(get_class_methods($tasks), function($m) {
+            return strpos($m, '__') === false;
+        });
+
+        foreach ($commandNames as $commandName) {
+            $command = $runner->createCommand(new TaskInfo('TaskFile', $commandName));
+            $command->setCode(function(InputInterface $input) use ($tasks, $commandName) {
+                // get passthru args
+                $args = $input->getArguments();
+                array_shift($args);
+                $args[] = $input->getOptions();
+                $res = call_user_func_array([$tasks, $commandName], $args);
+                if (is_int($res)) exit($res);
+                if (is_bool($res)) exit($res ? 0 : 1);
+                if ($res instanceof Result) exit($res->getExitCode());
+            });
+
+            $commands[] = $command;
+        }
+
+        return $commands;
     }
 }
